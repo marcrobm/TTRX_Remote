000fc041b2b9388b143e3331e1ea759771b96e6f
Marc Majohr <m.majohr@tu-braunschweig.de>
2022-10-06 11:42:03 +0200

    removed no longer required base64 function

--- main/drivers/TTRX_T30X.cpp
+++ main/drivers/TTRX_T30X.cpp
@@ -107,52 +107,6 @@ namespace TTRX_Remote
             }
         }
     }
-    //Function base64_encode taken from https://stackoverflow.com/questions/180947/base64-decode-snippet-in-c
-    static const std::string base64_chars =
-             "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-             "abcdefghijklmnopqrstuvwxyz"
-             "0123456789+/";
-    typedef unsigned char BYTE;
-    std::string base64_encode(BYTE const* buf, unsigned int bufLen) {
-  std::string ret;
-  int i = 0;
-  int j = 0;
-  BYTE char_array_3[3];
-  BYTE char_array_4[4];
-
-  while (bufLen--) {
-    char_array_3[i++] = *(buf++);
-    if (i == 3) {
-      char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
-      char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
-      char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
-      char_array_4[3] = char_array_3[2] & 0x3f;
-
-      for(i = 0; (i <4) ; i++)
-        ret += base64_chars[char_array_4[i]];
-      i = 0;
-    }
-  }
-
-  if (i)
-  {
-    for(j = i; j < 3; j++)
-      char_array_3[j] = '\0';
-
-    char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
-    char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
-    char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
-    char_array_4[3] = char_array_3[2] & 0x3f;
-
-    for (j = 0; (j < i + 1); j++)
-      ret += base64_chars[char_array_4[j]];
-
-    while((i++ < 3))
-      ret += '=';
-  }
-
-  return ret;
-}
     void T30X::printFrame(uint8_t* frame,uint32_t length){ 
         //std::cout << std::endl << base64_encode(frame, 1540) << std::endl;
         for(int i = 0;i<10;i++) {
